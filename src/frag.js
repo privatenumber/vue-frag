a300f162f2e3924604ed51b47427d102c5ab5453
Hiroki Osame <hiroki.osame@gmail.com>
2020-09-15 03:04:33 -0400

    perf: reuse patch methods

--- src/frag.js
+++ src/frag.js
@@ -1,10 +1,85 @@
+const $fakeParent = Symbol();
+
 function setFakeParent(node, fakeParent) {
-	if (!Object.getOwnPropertyDescriptor(node, 'parentNode')) {
+	if (!node[$fakeParent]) {
+		const {parentNode} = node;
+		node[$fakeParent] = fakeParent;
 		Object.defineProperty(node, 'parentNode', {
-			get: () => fakeParent,
+			get() {
+				return this[$fakeParent] || parentNode;
+			},
 		});
 	}
 }
+
+const $fakeChildren = Symbol();
+const parentPatches = {
+	insertBefore(newNode, refNode) {
+		const hasFakeChildren = this[$fakeChildren].get(refNode);
+		if (hasFakeChildren) {
+			refNode = hasFakeChildren[0];
+		}
+
+		return Element.prototype.insertBefore.call(this, newNode, refNode);
+	},
+	removeChild(node) {
+		const fc = this[$fakeChildren];
+		const hasFakeChildren = fc.get(node);
+		if (hasFakeChildren) {
+			node.append(...hasFakeChildren);
+			hasFakeChildren.splice(0);
+			fc.delete(node);
+			return;
+		}
+
+		return Element.prototype.removeChild.call(this, node);
+	},
+};
+
+function patchParent(parent, child, nodes) {
+	if (!parent[$fakeChildren]) {
+		parent[$fakeChildren] = new Map();
+		Object.assign(parent, parentPatches);
+	}
+
+	parent[$fakeChildren].set(child, nodes);
+}
+
+const elementPatches = {
+	insertBefore(newNode, refNode) {
+		const idx = this.frag.indexOf(refNode);
+		if (idx > -1) {
+			this.frag.splice(idx, 0, newNode);
+		}
+
+		const node = refNode.parentElement.insertBefore(newNode, refNode);
+		setFakeParent(node, this);
+		return node;
+	},
+
+	removeChild(node) {
+		const idx = this.frag.indexOf(node);
+		if (idx > -1) {
+			this.frag.splice(idx, 1);
+		}
+
+		return node.remove();
+	},
+
+	appendChild(node) {
+		const length_ = this.frag.length;
+		if (length_ > 0) {
+			const refNode = this.frag[length_ - 1];
+			refNode.after(node);
+		} else {
+			this.parentNode.append(node);
+		}
+
+		setFakeParent(node, this);
+
+		this.frag.push(node);
+	},
+};
 
 const frag = {
 	inserted(element) {
@@ -13,66 +88,18 @@ const frag = {
 		const {parentNode: parent} = element;
 
 		const fragment = document.createDocumentFragment();
-		fragment.append(...element.childNodes);
+		fragment.append(...nodes);
 		element.replaceWith(fragment);
 
 		element.frag = nodes;
 
-		const {insertBefore, removeChild} = parent;
-		Object.assign(parent, {
-			insertBefore(newNode, refNode) {
-				return insertBefore.call(parent, newNode, refNode === element ? nodes[0] : refNode);
-			},
-			removeChild(node) {
-				if (node === element) {
-					element.append(...element.frag);
-					element.frag.splice(0);
-					return;
-				}
-
-				return Reflect.apply(removeChild, this, arguments);
-			},
-		});
+		patchParent(parent, element, nodes);
 
 		setFakeParent(element, parent);
 
 		nodes.forEach(node => setFakeParent(node, element));
 
-		Object.assign(element, {
-			insertBefore(newNode, refNode) {
-				const idx = nodes.indexOf(refNode);
-				if (idx > -1) {
-					nodes.splice(idx, 0, newNode);
-				}
-
-				const node = refNode.parentElement.insertBefore(newNode, refNode);
-				setFakeParent(node, element);
-				return node;
-			},
-
-			removeChild(node) {
-				const idx = nodes.indexOf(node);
-				if (idx > -1) {
-					nodes.splice(idx, 1);
-				}
-
-				return node.remove();
-			},
-
-			appendChild(node) {
-				const length_ = nodes.length;
-				if (length_ > 0) {
-					const refNode = nodes[length_ - 1];
-					refNode.after(node);
-				} else {
-					parent.append(node); // Test
-				}
-
-				setFakeParent(node, element);
-
-				nodes.push(node);
-			},
-		});
+		Object.assign(element, elementPatches);
 	},
 };
 
