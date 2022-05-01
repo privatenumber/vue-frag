97e74e20bf8aa0fcf88c70978a7cd60c9214bc86
Robert Soriano <sorianorobertc@gmail.com>
2022-05-01 13:16:03 -0700

    fix: eslint having problems finding Fragment

--- frag.d.ts
+++ frag.d.ts
@@ -2,10 +2,14 @@ import Vue, { ComponentOptions } from 'vue';
 
 type FragmentComponent<V extends Vue = Vue> = ComponentOptions<V, never, never, never, never, never>;
 
+declare const fragment: FragmentComponent
+
 declare const frag: {
 	inserted(element: Element): void;
 	unbind(element: Element): void;
-	Fragment: FragmentComponent;
 };
 
-export = frag;
+export {
+	fragment as Fragment,
+	frag as default
+}
