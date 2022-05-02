3ca4938b3e03b08ecc231868862f4652814a18b4
Robert Soriano <sorianorobertc@gmail.com>
2022-05-02 15:40:42 -0700

    remove commit that breaks cjs imports

--- frag.d.ts
+++ frag.d.ts
@@ -2,14 +2,10 @@ import Vue, { ComponentOptions } from 'vue';
 
 type FragmentComponent<V extends Vue = Vue> = ComponentOptions<V, never, never, never, never, never>;
 
-declare const fragment: FragmentComponent
-
 declare const frag: {
 	inserted(element: Element): void;
 	unbind(element: Element): void;
+	Fragment: FragmentComponent;
 };
 
-export {
-	fragment as Fragment,
-	frag as default
-}
+export = frag;
