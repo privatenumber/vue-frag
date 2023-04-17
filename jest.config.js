d6a81e12e33299f1817f5508cf8e9ece19dcebf4
Hiroki Osame <hiroki.osame@gmail.com>
2023-04-17 12:37:13 +0900

    style: lint fix

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-undef
 module.exports = {
 	preset: 'es-jest',
 };
--- test/utils.ts
+++ test/utils.ts
@@ -95,6 +95,7 @@ export function dualMount<V extends Vue>(component: ComponentOptions<V>) {
 		frag,
 		normal,
 
+		// eslint-disable-next-line @typescript-eslint/ban-types
 		setData(data: object) {
 			return Promise.all([
 				normal.setData(data),
